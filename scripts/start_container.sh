82488cbbcf1c7cdb9e54fdb6cc212a2420465472
Dan Ardelean <dardelean@cloudbasesolutions.com>
2024-09-10 14:34:06 +0300

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+# Remove any existing container
+docker rm -f laughing_feistel || true
+
 # Pull the Docker image from Docker Hub
 docker pull dardelean/hotel-app:latest
 
 # Run the Docker image as a container
-docker run -dit -p 80:80 dardelean/hotel-app
+docker run -dit -p 80:80 --name hotel-app dardelean/hotel-app
